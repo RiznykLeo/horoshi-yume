e3d3af176ca0cd61be2e03b5bea00838da0f1aea
leonid riznyk <riznyk.leo@gmail.com>
2022-12-09 12:52:24 +0900

    complete navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
 function Navbar() {
-  return <div>I am Navbar</div>
+  return (
+    <div
+      className='grid grid-cols-3 items-center px-16 py-4
+     bg-zinc-50 drop-shadow-md sticky top-0'
+    >
+      <p className='uppercase font-bold text-2xl'>Horoshi Yume</p>
+      <ul className='flex justify-center gap-4 flex-wrap uppercase'>
+        <li>
+          <a href=''>Bio</a>
+        </li>
+        <li>
+          <a href=''>Gallery</a>
+        </li>
+        <li>
+          <a href=''>Manga</a>
+        </li>
+      </ul>
+      <div className='text-end '>
+        <a
+          className='px-4 py-2 bg-orange-500 text-white uppercase rounded'
+          href=''
+        >
+          Contact
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
